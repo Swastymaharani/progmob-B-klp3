78a0a97a802c58c08cda93b2d75979f1f061dcb0
SwastyMahaRanii913 <swastymaharani09@gmail.com>
2023-06-07 16:51:49 +0800

    first commit

---
+++ app/src/main/java/com/example/pemesananhotel/MulaiActivity.java
@@ -0,0 +1,14 @@
+package com.example.pemesananhotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MulaiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mulai);
+    }
+}
